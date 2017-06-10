e975e3971de18f261c098f787e2d93ca70acf051
Ítalo <italox131@gmail.com>
2017-06-10 17:00:59 +0200

    BluePrintSpawnableComponent()

--- BattleTanks/Source/BattleTanks/Public/TankAimingComponent.h
+++ BattleTanks/Source/BattleTanks/Public/TankAimingComponent.h
@@ -6,8 +6,10 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
-class UTankBarrel;// Forward declaration 
+// Forward declaration 
+class UTankBarrel;
 
+// Hold barrel's properties and Elevate method 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANKS_API UTankAimingComponent : public UActorComponent
 {
