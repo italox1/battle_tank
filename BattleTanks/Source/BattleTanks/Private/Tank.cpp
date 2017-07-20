61018a49ee164779f796c478940869a451dbfc59
Ítalo <italox131@gmail.com>
2017-07-20 11:12:59 +0200

    A Better Component Architecture

--- BattleTanks/Source/BattleTanks/Private/Tank.cpp
+++ BattleTanks/Source/BattleTanks/Private/Tank.cpp
@@ -18,7 +18,6 @@ ATank::ATank()
 	// No need to protect points as added at construction 
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 // Called when the game starts or when spawned
