f291a22d57d5480ff5a638e03c1f13ef6973d7b4
Ítalo <italox131@gmail.com>
2017-08-16 20:26:16 +0200

    OnComponentHit Event

--- BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankTrack.cpp
@@ -19,6 +19,16 @@ void UTankTrack::SetThrottle(float Throttle)
 }
 
 
+void UTankTrack::BeginPlay()
+{
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("I'm hit, I'm hit!"))
+}
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	// Calculate the slippage speed 
